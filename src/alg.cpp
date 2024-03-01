86d3405e7bf6a18702af6f461e43b85d22284821
UlianaKustova <91365332+UlianaKustova@users.noreply.github.com>
2024-03-01 15:42:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -27,7 +27,7 @@ uint64_t nPrime(uint64_t n) {
             return i;
         }
     }
-    return -1;
+    return 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
@@ -36,15 +36,15 @@ uint64_t nextPrime(uint64_t value) {
             return i;
         }
     }
-    return -1;
+    return 1;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t sum1 = 0;
+    uint64_t sum = 0;
     for (uint64_t i = 1; i <= hbound; i++) {
         if (checkPrime(i) == true) {
-            sum1 += i;
+            sum += i;
         }
     }
-    return sum1;
+    return sum;
 }
