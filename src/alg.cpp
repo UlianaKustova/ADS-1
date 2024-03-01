9c53a176b625cbbb460cbf81eafd8425383dab7d
UlianaKustova <91365332+UlianaKustova@users.noreply.github.com>
2024-03-01 15:14:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,47 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    bool isPrime = true;
+    if (value == 0 || value == 1 || value == 4) isPrime = false;
+    else {
+        for (int i = 2; i < value / 2; i++) {
+            if (value % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+    return isPrime;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    uint64_t s = 0;
+    for (uint64_t i = 2; i < 10000000; i++) {
+        if (checkPrime(i) == true) {
+            s++;
+        }
+        if (s == n) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    for (uint64_t i = value + 1; i < 10000000; i++) {
+        if (checkPrime(i) == true) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    uint64_t sum = 0;
+    for (uint64_t i = 1; i <= hbound; i++) {
+        if (checkPrime(i) == true) {
+            sum += i;
+        }
+    }
+    return sum;
 }
